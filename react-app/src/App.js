0f0dc0bfc9a442602f9e9c72822623ca127703ef
Si Hyeong Lee <super2451894@gmail.com>
2019-06-12 16:38:58 +0900

    State & Props & Key
    - State & Props & Key

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,14 +3,25 @@ import Navigation from './components/navigation';
 import Content from './components/content';
 import Subject from './components/subject';
 
-
-
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is HyperTEXT...'},
+        {id:2, title:'CSS', desc:'CSS is for design...'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive...'},
+        {id:4, title:'WOW', desc:'JavaScript is for interactive...'}
+      ]
+    }
+  }
   render(){
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web" />
-        <Navigation />
+        <Subject title={this.state.subject.title}
+        sub={this.state.subject.sub} />
+        <Navigation data={this.state.contents} />
         <Content title="HTML" desc="HTML is HyperText Markup Language."/>
       </div>
     );
