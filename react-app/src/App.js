5832124f427158c723f3bd1717841955068d2219
Si Hyeong Lee <super2451894@gmail.com>
2019-06-12 16:10:38 +0900

    Divide Components
    - Divide Components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,17 @@
-import React, {Component}from 'react';
+import React, { Component } from 'react';
+import Navigation from './components/navigation';
+import Content from './components/content';
+import Subject from './components/subject';
+
+
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        Hello, React!!
+        <Subject title="WEB" sub="world wide web" />
+        <Navigation />
+        <Content title="HTML" desc="HTML is HyperText Markup Language."/>
       </div>
     );
   }
